ad208675e243820fa1270551a2741fd2c7ccd7e2
k33g <ph.charriere@gmail.com>
2017-09-21 21:43:26 +0200

    v1

---
+++ src/main/java/org/typeunsafe/BaseStar.java
@@ -0,0 +1,40 @@
+package org.typeunsafe;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.BodyHandler;
+import java.util.Optional;
+
+public class BaseStar extends AbstractVerticle {
+
+
+  public void start() {
+
+    Router router = Router.router(vertx);
+    router.route().handler(BodyHandler.create());
+
+    router.get("/api/raiders").handler(context -> {
+      context.response()
+        .putHeader("content-type", "application/json;charset=UTF-8")
+        .end(new JsonObject().put("message", "hello").encodePrettily());
+    });
+
+    // serve static assets, see /resources/webroot directory
+    router.route("/*").handler(StaticHandler.create());
+
+    Integer httpPort = Integer.parseInt(Optional.ofNullable(System.getenv("PORT")).orElse("8080"));
+
+    HttpServer server = vertx.createHttpServer();
+
+    server.requestHandler(router::accept).listen(httpPort, result -> {
+      if(result.succeeded()) {
+        System.out.println("🌍 Listening on " + httpPort);
+      } else {
+        System.out.println("😡 Houston, we have a problem: " + result.cause().getMessage());
+      }
+    });
+  }
+}
